3c8b68f521f6d790042012a949f17d563196c936
HelmiYogantara <helmiyogantara@gmail.com>
2018-05-09 09:15:05 +0700

    sudah dirapikan

--- p0KelompokHewan.js
+++ p0KelompokHewan.js
@@ -1,53 +1,25 @@
 function groupAnimals(animals) {
   // you can only write your code here!
   animals.sort()
-  console.log(animals)
   var kamus = []
   var peternakan = []
 
   for (var i = 0; i < animals.length; i++) {
-    var index = kamus.indexOf(animals[i][0])
-    if (index === -1) {
-      kamus.push(animals[i][0])
-      peternakan.push([animals[i]])
-    } else {
-      peternakan[index].push(animals[i])
-    }
-  }
+    var index = kamus.indexOf(animals[i][0])  // index = kamus.indexof(a)
+    if (index === -1) {                       // = -1
+      kamus.push(animals[i][0])               // kamus = a
+      peternakan.push([animals[i]])           // ptr = [anoa]
+    } else {                                  //
+      peternakan[index].push(animals[i])      //
+    }                                         //
+  }                                           //
 
-  console.log(peternakan)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var beda = []
-  // var pisah = ''
-  // var awalslice = 0;
-  // for(var i = 0; i < animals.length-1; i++){
-  //   if(animals[i][0] !== animals[i+1][0]){
-  //     pisah = animals.slice(awalslice,i-awalslice)
-  //     beda.push(pisah)
-  //   }
-  // }
-  // console.log (beda)
-  // return beda
+  return peternakan
  
 }
 // TEST CASES
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil'])
+//console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 //console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
