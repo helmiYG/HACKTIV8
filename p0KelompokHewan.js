ca22ef59dd9691b1a79af014835e115bf128ef46
HelmiYogantara <helmiyogantara@gmail.com>
2018-05-09 09:17:26 +0700

    sudah dirapikan

--- p0KelompokHewan.js
+++ p0KelompokHewan.js
@@ -5,21 +5,21 @@ function groupAnimals(animals) {
   var peternakan = []
 
   for (var i = 0; i < animals.length; i++) {
-    var index = kamus.indexOf(animals[i][0])  // index = kamus.indexof(a)
-    if (index === -1) {                       // = -1
-      kamus.push(animals[i][0])               // kamus = a
-      peternakan.push([animals[i]])           // ptr = [anoa]
-    } else {                                  //
-      peternakan[index].push(animals[i])      //
-    }                                         //
-  }                                           //
+    var index = kamus.indexOf(animals[i][0])  
+    if (index === -1) {                      
+      kamus.push(animals[i][0])               
+      peternakan.push([animals[i]])           
+    } else {                                  
+      peternakan[index].push(animals[i])      
+    }                                         
+  }                                         
 
   return peternakan
  
 }
 // TEST CASES
 groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil'])
-//console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-//console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+[ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+[ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
